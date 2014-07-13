f56c2a111411b817973495ff0a9ac4c259e58605
Vic Schaeperkoetter <victor.schaeperkoetter@gmail.com>
2014-07-13 19:03:14 -0400

    Implemented CSS Layout on Main Page, fix formula to correctly calculate weekly values, added Nav Bar, Logo, Links to financial websites and select tags.

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -4,10 +4,12 @@ get '/' do
 end
 
 post '/result' do
+	frequency = params[:frequency].to_i
 	principal = params[:principal].to_i
   contribution = params[:contribution].to_i
   rate =  params[:rate].to_i
   years = params[:years].to_i
-  result = ( principal * (rate/100.0 + 1)**years ) + ( contribution * ( ( (rate/100.0) + 1)**years )/((rate/100.0)) )
+	result = ( principal * (rate/100.0 + 1)**years ) + ( contribution/frequency *((1 + rate/100.0)**years-1)/( (1+(rate/100.0))**(1.0/frequency)-1) )
   "The Portfolio will be worth: $" + result.floor.to_s.gsub(/(\d)(?=(\d\d\d)+(?!\d))/, "\\1,")
 end
+
