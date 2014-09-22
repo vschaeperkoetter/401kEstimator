67db571256c40bb537ed21b2e97d53c841c6c918
Vic Schaeperkoetter <victor.schaeperkoetter@gmail.com>
2014-09-22 12:49:04 -0400

    Update index.rb

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -4,14 +4,13 @@ get '/' do
 end
 
 post '/result' do
-	frequency = params[:frequency].to_i
-	principal = params[:principal].to_i
+  frequency = params[:frequency].to_i
+  principal = params[:principal].to_i
   contribution = params[:contribution].to_i
   rate =  params[:rate].to_i
   years = params[:years].to_i
-	result = ( principal * (rate/100.0 + 1)**years ) + ( contribution/frequency *((1 + rate/100.0)**years-1)/( (1+(rate/100.0))**(1.0/frequency)-1) )
-	@result = "$" + result.floor.to_s.gsub(/(\d)(?=(\d\d\d)+(?!\d))/, "\\1,")
-  # "The Portfolio will be worth: $" + result.floor.to_s.gsub(/(\d)(?=(\d\d\d)+(?!\d))/, "\\1,")
+  result = ( principal * (rate/100.0 + 1)**years ) + ( contribution/frequency *((1 + rate/100.0)**years-1)/( (1+(rate/100.0))**(1.0/frequency)-1) )
+  @result = "$" + result.floor.to_s.gsub(/(\d)(?=(\d\d\d)+(?!\d))/, "\\1,")
   erb :result
 end
 
